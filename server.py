48751798ab8d020eabd76656c3caac356490cdf0
MadTofu22 <stutler.tom@gmail.com>
2020-12-04 14:10:23 -0600

    added put route for checking pets in and out

--- server.py
+++ server.py
@@ -21,3 +21,24 @@ def pets_route():
         data = cursor.fetchall()
         print('data from pets GET:', data)
         return jsonify(data)
+
+# Route for the Pets table with paramater for pet id
+@app.route('/api/pets/<int:pet_id>', methods=['PUT'])
+def pet_byId_route(pet_id):
+    try:
+        if request.method == 'PUT':
+            req = request.get_json()
+            if req['checkDirection'] == 'OUT':
+                sql = 'UPDATE "pets" SET "is_checked_in" = false WHERE "id" = %s'
+            elif req['checkDirection'] == 'IN':
+                sql = 'UPDATE "pets" SET "is_checked_in" = true WHERE "id" = %s'
+            else:
+                return 'Something has gone wrong', 501
+            
+            cursor.execute(sql, (pet_id,))
+            conn.commit()
+            return 'Request OK', 200
+
+    except(Exception, psycopg2.Error) as error:
+        print("Error connecting to PostgreSQL database", error)
+        return 'Bad Request %s' % error, 500

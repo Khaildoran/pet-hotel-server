898d1947801db2a6edd9183c013abe41b7468f1d
lawlernp <lawlernp@gmail.com>
2020-12-04 14:04:38 -0600

    owners route for POST in server.py

--- server.py
+++ server.py
@@ -22,3 +22,19 @@ def pets_route():
         data = cursor.fetchall()
         print('data from pets GET:', data)
         return jsonify(data)
+
+# Route for the Owners table
+@app.route('/api/owners', methods=['POST'])
+def owners_route():
+    # handle the POST request
+    if request.method == 'POST':
+        try:
+            req = request.get_json()
+            SQL = 'INSERT INTO "owners" ("first_name", "last_name") VALUES (%s, %s);'
+            data = [req['first_name'], req['last_name']]
+            cursor.execute(SQL, data)
+            conn.commit()
+            print('in owners POST')
+            return 'OK'
+        except(Exception, psycopg2.Error) as error:
+            print("Error connecting to PostgreSQL database", error)
